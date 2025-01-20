d5d69a48d2544ceabf77f4a9eb947a08f3fa79d1
CodingRainbowCat <141959939+CodingRainbowCat@users.noreply.github.com>
2025-01-20 12:57:12 -0300

    Add assertion on the dummy test

--- framework/src/tests/Dummy.spec.ts
+++ framework/src/tests/Dummy.spec.ts
@@ -1,4 +1,9 @@
+import { should } from 'chai';
+should();
+
 describe('Pipeline initial tests', () => {
   it('Dummy success test', () => {
+    var result = true;
+    result.should.be.true;
   });
 });
